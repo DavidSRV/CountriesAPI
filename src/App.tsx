ec03e4bcd4001d85c899c7eab6422bdd385dd7b2
David Ruiz <david.ruizvargas@hotmail.com>
2023-03-12 22:47:03 -0500

    Hook useFecth and Api done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import NavBar from "./components/navbar/NavBar"
-import './sass/resets.scss'
+import NavBar from "./components/navbar/NavBar";
+import { useFetch } from "./hooks/useFetch";
+import "./sass/resets.scss";
 
 function App() {
+  const [countries, isLoading, error] = useFetch(
+    "https://restcountries.com/v3.1/all"
+  );
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (error) return <div>Error: {error.message}</div>;
+
   return (
     <div className="App">
-     <NavBar></NavBar>
+      <NavBar />
+      <ul>
+        {countries &&
+          countries.map((country) => (
+            <div>
+              <li key={country.name.common}>{country.name.common}</li>
+              <li key={country.name.common}><img src={`${country.flags.png}`} alt="" /></li>
+            </div>
+          ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+
+interface ApiError extends Error {
+  status: number;
+  statusText: string;
+}
+
+interface ApiCountry {
+  name: {
+    common: string;
+  };
+  capital: string[];
+  region: string;
+  population: number;
+  flags: {
+    png: string
+    alt: string
+  }
+}
+
+async function fetchCountries(url: string): Promise<ApiCountry[]> {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+export function useFetch(
+  url: string
+): [ApiCountry[] | null, boolean, Error | null] {
+  const [data, setData] = useState<ApiCountry[] | null>(null);
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsLoading(true);
+      try {
+        const countries = await fetchCountries(url);
+        setData(countries);
+      } catch (error: unknown) {
+        const apiError = error as ApiError;
+        setError(apiError);
+      }
+      setIsLoading(false);
+    }
+
+    fetchData();
+  }, [url]);
+
+  return [data, isLoading, error];
+}
