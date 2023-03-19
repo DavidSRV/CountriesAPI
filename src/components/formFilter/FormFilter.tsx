877b05450ffc97ffbf3b3a46aa7f098131b32ba2
David Ruiz <david.ruizvargas@hotmail.com>
2023-03-19 13:38:46 -0500

    add filters

--- src/components/formFilter/FormFilter.tsx
+++ src/components/formFilter/FormFilter.tsx
@@ -1,14 +1,29 @@
 import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import "./formFilter.scss";
 
 const FormFilter = () => {
   return (
     <form action="#">
-      <div>
+      <div className="form__containerFilterCountry">
+        <SearchIcon className="form__icon" />
         <input
+          className="form__inputText"
           type="text"
           placeholder="Search for a country"
-          style={{ border: "none", outline:"none"}}
         />
+      </div>
+      <div className="form__containerFilterRegion">
+        <div className="form__labelIcon">
+          <label htmlFor="region">Filter by Region</label>
+        </div>
+        <select name="region">
+          <option value="Africa">Africa</option>
+          <option value="America">America</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
+        </select>
       </div>
     </form>
   );
